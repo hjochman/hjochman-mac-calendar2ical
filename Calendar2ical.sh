61acfa42084028e2056a9a8966b362cf88258014
hjochman <38923044+hjochman@users.noreply.github.com>
2023-05-12 14:31:25 +0200

    Bugfixing

--- Calendar2ical.sh
+++ Calendar2ical.sh
@@ -5,7 +5,7 @@
 # Company: ITEna Solutions
 # Creation Date: 06 March 2012
 
-# iOS Calendar to ical converter
+# MacOS Calendar to ical converter
 # Author: Hanspeter Jochmann
 # Creation Date: Mai 2023
 
@@ -65,6 +65,24 @@ function output_ical_header() {
 BEGIN:VCALENDAR
 VERSION:2.0
 PRODID:hp-jochmann.de/mac-calendar2ical v1.0
+CALSCALE:GREGORIAN
+METHOD:PUBLISH
+BEGIN:VTIMEZONE
+TZID:Europe/Berlin
+LAST-MODIFIED:20230411T190536Z
+BEGIN:STANDARD
+DTSTART:20051030T010000
+TZOFFSETTO:+0100
+TZOFFSETFROM:+0000
+TZNAME:CET
+END:STANDARD
+BEGIN:DAYLIGHT
+DTSTART:20060326T030000
+TZOFFSETTO:+0200
+TZOFFSETFROM:+0100
+TZNAME:CEST
+END:DAYLIGHT
+END:VTIMEZONE
 ENDOFTEXT
 }
 
@@ -85,7 +103,7 @@ extractField () {
 # Return database field. Fields are seperated with '|'
   FIELD=$1
   POSITION=$2
-  echo ${FIELD} | cut -d 'œ' -f $POSITION
+  echo ${FIELD} | cut -d '|' -f $POSITION
 }
 
 convertMacAbsoluteTimeOld () {
@@ -105,7 +123,7 @@ convertMacAbsoluteTime () {
 # sqlite3 requires a database file to be specified to executed queries
   MAC=$1
   MAC_ROUND=`truncateFloat ${MAC}`
-  echo `date -r ${MAC_ROUND} -v+31y +%Y%m%dT%H%M%SZ`
+  echo `date -r ${MAC_ROUND} -v+31y -v+1d +%Y%m%dT%H%M%SZ`
 }
 
 ######  Main ################
@@ -113,13 +131,13 @@ CALENDAR_START_MAC=$(date -r ${CALENDAR_START} -v-31y +%s)
 CALENDAR_END_MAC=$(date -r ${CALENDAR_END} -v-31y +%s)
 
 cp "${CALENDAR_DB_SOURCE}" "${CALENDAR_DB_DESTINATION}"
-CALENDAR_ITEMS=`sqlite3 -separator "œ" "${CALENDAR_DB_DESTINATION_FILE}" "select summary,start_date,end_date,last_modified,unique_identifier,location_id,ROWID,has_recurrences,all_day from CalendarItem where calendar_id = ${CALENDAR_ID};"`
+CALENDAR_ITEMS=`sqlite3 "${CALENDAR_DB_DESTINATION_FILE}" "select start_date,end_date,last_modified,unique_identifier,location_id,ROWID,has_recurrences,all_day from CalendarItem where calendar_id = ${CALENDAR_ID};"`
 
 #For debug get cal entry by ID
-#CALENDAR_ITEMS=`sqlite3 "${CALENDAR_DB_DESTINATION_FILE}" "select summary,start_date,end_date,last_modified,unique_identifier,location_id,ROWID,has_recurrences,all_day from CalendarItem where ROWID = 1699;"`
+#CALENDAR_ITEMS=`sqlite3 "${CALENDAR_DB_DESTINATION_FILE}" "select start_date,end_date,last_modified,unique_identifier,location_id,ROWID,has_recurrences,all_day from CalendarItem where ROWID = 1699;"`
 
 
-#sqlite3 "${CALENDAR_DB_DESTINATION_FILE}" "select summary,start_date,end_date,last_modified,unique_identifier,location_id,calendar_id,has_recurrences,UUID from CalendarItem where calendar_id = 8;"
+#sqlite3 "${CALENDAR_DB_DESTINATION_FILE}" "select start_date,end_date,last_modified,unique_identifier,location_id,calendar_id,has_recurrences,UUID from CalendarItem where calendar_id = 8;"
 #sqlite3 "${CALENDAR_DB_DESTINATION_FILE}" "select frequency,UUID,owner_id from Recurrence;" # where UUID = \"31C6C1D6-0AEB-44B5-9E92-D6448EB6BBD2\";"
 #exit
 
@@ -134,16 +152,15 @@ output_ical_header
 
 for calendarItem in ${CALENDAR_ITEMS[@]}
 do
-  #echo "==========> ${CALENDAR_START_MAC}-${CALENDAR_END_MAC} DEBUG: ${calendarItem}"
-  SUMMARY=`extractField ${calendarItem} 1 | tr -d '\r'`
-  START_DATE_RAW=`extractField ${calendarItem} 2`
-  END_DATE_RAW=`extractField ${calendarItem} 3`
-  LAST_MODIFIED_RAW=`extractField ${calendarItem} 4`
-  ITEM=`extractField ${calendarItem} 5`
-  LOC_ID=`extractField ${calendarItem} 6`
-  EVENT_ID=`extractField ${calendarItem} 7`
-  HAS_OC=`extractField ${calendarItem} 8`
-  ALL_DAY=`extractField ${calendarItem} 9`
+  #echo "==========> ${CALENDAR_START_MAC}<${START_DATE_RAW%.*}<${CALENDAR_END_MAC} DEBUG: ${calendarItem}"
+  START_DATE_RAW=`extractField ${calendarItem} 1`
+  END_DATE_RAW=`extractField ${calendarItem} 2`
+  LAST_MODIFIED_RAW=`extractField ${calendarItem} 3`
+  ITEM=`extractField ${calendarItem} 4`
+  LOC_ID=`extractField ${calendarItem} 5`
+  EVENT_ID=`extractField ${calendarItem} 6`
+  HAS_OC=`extractField ${calendarItem} 7`
+  ALL_DAY=`extractField ${calendarItem} 8`
  
   #echo "==========> DEBUG: conv start"
   START_DATE=`convertMacAbsoluteTime $START_DATE_RAW`
@@ -156,25 +173,27 @@ do
     LAST_MODIFIED=`convertMacAbsoluteTime $LAST_MODIFIED_RAW`
   fi
 #  LOC=`sqlite3 -separator " " "${CALENDAR_DB_DESTINATION_FILE}" "select title, address from Location where item_owner_id = '${LOC_ID}';" | tr -d '\r'`
-  LOC=`sqlite3 -separator " " "${CALENDAR_DB_DESTINATION_FILE}" "select title, address from Location where ROWID = '${LOC_ID}';" | tr -d '\r'`
-  DESC=`sqlite3 "${CALENDAR_DB_DESTINATION_FILE}" "select description from CalendarItem where unique_identifier = '${ITEM}';" | tr -d '\r'` 
+  LOC=`sqlite3 -separator " " "${CALENDAR_DB_DESTINATION_FILE}" "select title, address from Location where ROWID = '${LOC_ID}';" | tr -dc '\007-\011\012-\014\040-\376'`
+  DESC=`sqlite3 "${CALENDAR_DB_DESTINATION_FILE}" "select description from CalendarItem where unique_identifier = '${ITEM}';" | tr -dc '\007-\011\012-\014\040-\376'` 
+  SUMMARY=`sqlite3 "${CALENDAR_DB_DESTINATION_FILE}" "select summary from CalendarItem where unique_identifier = '${ITEM}';" | tr -dc '\007-\011\012-\014\040-\376'` 
+  #echo "==========> ${SUMMARY}"
 
   if [[ $HAS_OC == "0" ]]; then
     if [[ ${CALENDAR_START_MAC} -le ${START_DATE_RAW%.*} ]] && [[ ${CALENDAR_END_MAC} -ge ${START_DATE_RAW%.*} ]] ; then
-      printf "BEGIN:VEVENT\nUID:%s\nDTSTAMP:%s\n" \
+      printf "BEGIN:VEVENT\nUID:%s\nDTSTAMP;TZID=Europe/Berlin:%s\n" \
              "${ITEM}" "${LAST_MODIFIED}" 
       if [[ $ALL_DAY == "1" ]]; then
-        printf "DTSTART;VALUE=DATE:%s\n" \
+        printf "DTSTART;TZID=Europe/Berlin;VALUE=DATE:%s\n" \
                "${START_DATE:0:8}" 
       else
-        printf "DTSTART:%s\nDTEND:%s\n" \
+        printf "DTSTART;TZID=Europe/Berlin:%s\nDTEND;TZID=Europe/Berlin:%s\n" \
                "${START_DATE}" "${END_DATE}"
       fi
       printf "SUMMARY:%s\nLOCATION:%s\nDESCRIPTION:%s\nEND:VEVENT\n" \
              "${SUMMARY//$'\n'/\\n}" "${LOC//$'\n'/\\n}" "${DESC//$'\n'/\\n}"
     fi        
   else
-    OCCURRENC_ITEMS=`sqlite3 -separator "œ" "${CALENDAR_DB_DESTINATION_FILE}" "select day,occurrence_date,occurrence_start_date,occurrence_end_date from OccurrenceCache where event_id = ${EVENT_ID};"`        
+    OCCURRENC_ITEMS=`sqlite3 "${CALENDAR_DB_DESTINATION_FILE}" "select day,occurrence_date,occurrence_start_date,occurrence_end_date from OccurrenceCache where event_id = ${EVENT_ID};"`        
     for ocItem in ${OCCURRENC_ITEMS[@]}
     do
       OC_DAY_RAW=`extractField ${ocItem} 1`
@@ -192,14 +211,15 @@ do
       #echo $OC_DATE
       #echo $OC_START_DATE
       #echo $OC_END_DATE
-      if [[ ${CALENDAR_START_MAC} -le ${START_DATE_RAW%.*} ]] && [[ ${CALENDAR_END_MAC} -ge ${START_DATE_RAW%.*} ]] ; then
-        printf "BEGIN:VEVENT\nUID:%s\nDTSTAMP:%s\n" \
+      #echo "==========> ${CALENDAR_START_MAC}<${OC_DATE_RAW%.*}<${CALENDAR_END_MAC} DEBUG_OC: ${ocItem}"
+      if [[ ${CALENDAR_START_MAC} -le ${OC_DATE_RAW%.*} ]] && [[ ${CALENDAR_END_MAC} -ge ${OC_DATE_RAW%.*} ]] ; then
+        printf "BEGIN:VEVENT\nUID:%s\nDTSTAMP;TZID=Europe/Berlin:%s\n" \
                "${ITEM}-${OC_DATE}" "${LAST_MODIFIED}" 
         if [[ $ALL_DAY == "1" ]]; then
-          printf "DTSTART;VALUE=DATE:%s\n" \
+          printf "DTSTART;TZID=Europe/Berlin;VALUE=DATE:%s\n" \
                  "${OC_DATE:0:8}" 
         else
-          printf "DTSTART:%s\nDTEND:%s\n" \
+          printf "DTSTART;TZID=Europe/Berlin:%s\nDTEND;TZID=Europe/Berlin:%s\n" \
                  "${OC_DATE}" "${OC_END_DATE}" 
         fi
         printf "SUMMARY:%s\nLOCATION:%s\nDESCRIPTION:%s\nEND:VEVENT\n" \
